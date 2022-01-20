300c708c2174c7588ba51d7cf5c43c45d94f6a14
gitblairshub <blairsarlo@gmail.com>
2022-01-20 17:10:39 -0500

    Added state for clicking menu icon and ul. Add style to nav-menu and nav-links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,25 @@ import { MenuItems } from './MenuItems';
 import './Navbar.css';
 
 class Navbar extends Component {
+  state = { clicked: false }
+
+  handleClick = () => {
+  // kind of like a toggle. bars changes to times on click and vice versa 
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+
+
   render() {
     return(
       <nav className="NavbarItems">
         <h1 className="navbar-logo">React<i className='fab fa-react'></i></h1>
-        <div className="menu-icon">
-
+        <div className="menu-icon" onClick={this.handleClick}>
+          {/* if the menu icon is clicked - then show times otherwise display bars */}
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i> 
         </div>
-        <ul>
+        {/* for mobile view - if clicked add className active to the Ul, otherwise display nav-menu  */}
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
